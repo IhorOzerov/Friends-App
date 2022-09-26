5ffe0bed5450a8b84acb3cac4e8d91783169c3d9
Ihor <goshus@ukr.net>
2022-09-26 18:07:01 +0300

    final checked version

--- script.js
+++ script.js
@@ -6,68 +6,66 @@ function handleErrors(response) {
 }
 
 const url = 'https://randomuser.me/api/?nat=ua&results=24&inc=gender,name,picture,dob,cell&noinfo';
-const respData = []
-
+const responseData = []
 const mainContainer = document.querySelector('.container')
 const listOfMembers = document.querySelector(".list");
 
 const getData = (url) => {
-  
   try {
     fetch(url)  
-    .then(handleErrors)
+      .then(handleErrors)
       .then(response => response.json())
       .then((json) => {
-        respData.push(...json.results);
-        getPeoples(respData);
+        responseData.push(...json.results);
+        getPeoples(responseData);
       });
   } catch (err) {
-    alert("Try again please");
      console.log(err);
      mainContainer.innerHTML = `<div class="errorScreen">
      <p class="errorText">Something wrong with connect, try to refresh the page</p>
+     <p class="errorText">${err}</p>
      <img class="errorImage" src="./images/err.png" alt="error"></div>`
   }
 }
 getData(url);
 
-function getPeoples(respData) {
-  respData.forEach((user) => {
+function getPeoples(responseData) {
+  responseData.forEach((user) => {
     const newCard = document.createElement('div');
     newCard.classList.add('human');
     listOfMembers.appendChild(newCard);
     newCard.innerHTML = `
       <img class="myFace" src="${user.picture.large}" alt="myFace">
-      <div class="color"></div>
-      <p class="myGender">${user.gender}</p>
-      <p class="myName">${user.name.first} ${user.name.last}</p>
-      <p class="myAge">${user.dob.age}</p>
+      <div class="profileColor"></div>
+      <p class="myData">${user.gender}</p>
+      <p class="myData">${user.name.first} ${user.name.last}</p>
+      <p class="myData">${user.dob.age} y.o.</p>
       <a href="tel:${user.cell.replace(/[A-Z]/, Math.floor(Math.random()*9))}">
       ${user.cell.replace(/[A-Z]/, Math.floor(Math.random()*9))}</a>`
   });
 }
 
-const findThat = document.querySelector("#searchFriend");
+const searchInput = document.querySelector("#searchFriend");
 const genderSelector = document.querySelector('#sex');
 const sortingSelector = document.querySelector("#ageAndName");
 
-findThat.addEventListener("input", sortMembers);
+searchInput.addEventListener("input", sortMembers);
 genderSelector.addEventListener("change", sortMembers);
 sortingSelector.addEventListener("change", sortMembers);
 
 function sortMembers() {
-  let sortedMembers = [...respData]
+  let sortedMembers = [...responseData]
 
-  sortedMembers = sortedMembers.filter(element => `${element.name.first}${element.name.last}`
+  sortedMembers = sortedMembers.filter(human => `${human.name.first}${human.name.last}`
   .toLowerCase()
-  .includes(findThat.value.trim()));
+  .includes(searchInput.value.trim()));
   
   if (genderSelector.value === "All") {
     sortedMembers;
   } else if (genderSelector.value === "Female") {
-    sortedMembers = sortedMembers.filter(element => element.gender === "female")
+    sortedMembers = sortedMembers.filter(human => human.gender === "female")
   } else {
-    sortedMembers = sortedMembers.filter(element => element.gender === "male")
+    sortedMembers = sortedMembers.filter(human => human.gender === "male")
   }
 
   if (sortingSelector.value === 'upAge') {
@@ -86,7 +84,7 @@ function sortMembers() {
 }
 
 function sortingNames(a, b){
- return a.name.first == b.name.first ? 0 : a.name.first < b.name.first ? -1 : 1;
+  return a.name.first == b.name.first ? 0 : a.name.first < b.name.first ? -1 : 1;
 }
 
 function sortingAge(a, b){
